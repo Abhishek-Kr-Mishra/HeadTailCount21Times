a122af1871acc6301166a99ccaf604950ce31f71
abh224272 <abh224272@gmail.com>
2020-11-20 17:38:10 +0530

    Added: Condition to run the Toos loop

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -4,11 +4,16 @@
 echo "********Welcome to Coin toss Simulation*********"
 headValue=1
 tailValue=2
-
+headCount=0
+tailCount=0
+		while (( $headCount!=10 && $tailCount!=10 ))
+		do
                 toss=$(( RANDOM%2+1 ))
                 if (( $toss==$headValue ))
                 then
-                        echo "It's Head.....Head is winner"
+                        ((headCount++))
                 else
-			echo "It's a Tail....Tail is Winner"
+			((tailCount++))
                 fi
+		done
+		echo "Head has won $headCount times and Tail has won $tailCount times"
